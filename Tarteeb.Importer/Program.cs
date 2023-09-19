188aba71061ef5558f267f1aaa7d92b37fa057f8
Zafar <icom@MACKBOOK>
2023-09-19 18:11:14 +0300

    CODE RUB: Add Header

---
+++ Tarteeb.Importer/Program.cs
@@ -0,0 +1,15 @@
+﻿//=================================
+// Copyright (c) Tarteeb LLC.
+// Powering True Leadership
+//===============================
+
+namespace Tarteeb.Importer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
