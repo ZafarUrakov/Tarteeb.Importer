07ad16f679f37f1268cd08113d46503f351b30d3
Zafar <icom@MACKBOOK>
2023-09-19 22:08:22 +0300

    DATA: Model Client Create

---
+++ Tarteeb.Importer/Models/Clients/Client.cs
@@ -0,0 +1,19 @@
+﻿//=================================
+// Copyright (c) Tarteeb LLC.
+// Powering True Leadership
+//===============================
+using System;
+
+namespace Tarteeb.Importer.Models.Clients
+{
+    internal class Client
+    {
+        public Guid Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public DateTimeOffset BirthDate { get; set; }
+        public Guid GroupId { get; set; }
+    }
+}
