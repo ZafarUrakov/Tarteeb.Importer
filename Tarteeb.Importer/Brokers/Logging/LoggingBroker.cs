e21e38104de6abda0a613854803906a7452e9095
Zafar <icom@MACKBOOK>
2023-09-21 18:15:47 +0300

    CODE RUB: Run Code Cleanup

--- Tarteeb.Importer/Brokers/Logging/LoggingBroker.cs
+++ Tarteeb.Importer/Brokers/Logging/LoggingBroker.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Tarteeb.Importer.Models.Clients.Exceptions;
 
 namespace Tarteeb.Importer.Brokers.Logging
