e97dc3b1871102df6853607923e3bdb4de5e20e0
Your Name <you@example.com>
2023-09-22 00:54:21 +0500

    POC: Add Client Service

---
+++ Tarteeb.Importer/Services/Clients/ClientService.cs
@@ -0,0 +1,36 @@
+﻿//=================================
+// Copyright (c) Tarteeb LLC.
+// Powering True Leadership
+//===============================
+
+
+using System.Threading.Tasks;
+using Tarteeb.Importer.Brokers.Storages;
+using Tarteeb.Importer.Models.Clients;
+using Tarteeb.Importer.Models.Clients.Exceptions;
+using Tarteeb.Importer.Brokers.Logging;
+
+namespace Tarteeb.Importer.Services.Clients
+{
+    internal class ClientService
+    {
+        private readonly StorageBroker storageBroker;
+        private readonly LoggingBroker loggingBroker;
+
+        public ClientService()
+        {
+            this.storageBroker = new StorageBroker();
+            this.loggingBroker = new LoggingBroker();
+        }
+
+        internal Task<Client> AddClientAsync(Client client)
+        {
+            if (client == null)
+            {
+                loggingBroker.LoggingError(new NullClientException());
+            }
+
+            return this.storageBroker.InsertClientAsync(client);
+        }
+    }
+}
