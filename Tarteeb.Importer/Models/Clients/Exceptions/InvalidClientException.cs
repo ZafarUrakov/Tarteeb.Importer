c3da56175de340207a3c258883a55732d631df2b
Zafar <icom@MACKBOOK>
2023-09-23 22:12:05 +0300

    FOUNDATIONS: Add Invalid Client Exception

---
+++ Tarteeb.Importer/Models/Clients/Exceptions/InvalidClientException.cs
@@ -0,0 +1,16 @@
+﻿//=================================
+// Copyright (c) Tarteeb LLC.
+// Powering True Leadership
+//===============================
+
+using Xeptions;
+
+namespace Tarteeb.Importer.Models.Clients.Exceptions
+{
+    internal class InvalidClientException : Xeption
+    {
+        public InvalidClientException()
+            : base(message: "Client is invalid. Fix the error and try again.")
+        { }
+    }
+}
