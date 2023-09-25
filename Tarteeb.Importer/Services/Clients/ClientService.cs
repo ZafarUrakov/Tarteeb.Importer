72ab02addf9a9be2e6e6278cafc011c42873261b
Zafar <icom@MACKBOOK>
2023-09-25 22:13:22 +0300

    POC: Add Client(WIP)

--- Tarteeb.Importer/Services/Clients/ClientService.cs
+++ Tarteeb.Importer/Services/Clients/ClientService.cs
@@ -4,7 +4,9 @@
 //===============================
 
 using System;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using Tarteeb.Importer.Brokers.DataTimes;
 using Tarteeb.Importer.Brokers.Logging;
 using Tarteeb.Importer.Brokers.Storages;
 using Tarteeb.Importer.Models.Clients;
@@ -12,7 +14,7 @@ using Tarteeb.Importer.Models.Clients.Exceptions;
 
 namespace Tarteeb.Importer.Services.Clients
 {
-    internal class ClientService
+    internal class ClientService : DateTimeBroker
     {
         private readonly StorageBroker storageBroker;
         private readonly LoggingBroker loggingBroker;
@@ -31,22 +33,41 @@ namespace Tarteeb.Importer.Services.Clients
             }
 
             Validate(
-                (Rule: Isinvalid(client.Id), Parameter: nameof(client.Id)),
-                (Rule: Isinvalid(client.Firstname), Parameter: nameof(client.Id)));
+                (Rule: IsInvalid(client.Id), Parameter: nameof(client.Id)),
+                (Rule: IsInvalid(client.Firstname), Parameter: nameof(client.Firstname)),
+                (Rule: IsInvalid(client.Lastname), Parameter: nameof(client.Lastname)),
+                (Rule: IsInvalid(client.Email), Parameter: nameof(client.Email)));
+
+            Validate(
+                (Rule: IsInvalidBirthData(client.BirthDate), Parameter: nameof(client.BirthDate)),
+                (Rule: IsInvalidEmail(client.Email), Parameter: nameof(client.Email)),
+                (Rule: IsInvalidPhoneNumber(client.PhoneNumber), Parameter: nameof(client.PhoneNumber)));
 
             return this.storageBroker.InsertClientAsync(client);
         }
 
-        private dynamic Isinvalid(Guid id) => new
+        private dynamic IsInvalid(Guid id) => new
         {
             Condition = id == default,
             Message = "Id is required"
         };
 
-        private dynamic Isinvalid(string text) => new
+        private dynamic IsInvalid(string text) => new
         {
             Condition = String.IsNullOrWhiteSpace(text),
             Message = "Text is required"
+        };
+
+        private dynamic IsInvalidEmail(string email) => new
+        {
+            Condition = Regex.IsMatch(email, @"^(.+)@(.+)$"),
+            Message = "Email is invalide"
+        };
+
+        private dynamic IsInvalidPhoneNumber(string phoneNumber) => new
+        {
+            Condition = Regex.IsMatch(phoneNumber, "^\\+?[1-9][0-9]{7,14}$"),
+            Message = "Phone number is invalide"
         };
 
         private void Validate(params (dynamic Rule, string Parameter)[] validations)
