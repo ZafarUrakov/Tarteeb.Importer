305acac64ea3391bb26bd604b07ebb0b076d3a28
Zafar <icom@MACKBOOK>
2023-09-25 22:42:55 +0300

    POC: Add Client(WIP)

--- Tarteeb.Importer/Services/Clients/ClientService.cs
+++ Tarteeb.Importer/Services/Clients/ClientService.cs
@@ -3,6 +3,7 @@
 // Powering True Leadership
 //===============================
 
+using System;
 using System.Threading.Tasks;
 using Tarteeb.Importer.Brokers.Logging;
 using Tarteeb.Importer.Brokers.Storages;
@@ -29,7 +30,42 @@ namespace Tarteeb.Importer.Services.Clients
                 loggingBroker.LoggingError(new NullClientException());
             }
 
+            Validate(
+                (Rule: IsInvalid(client.Id), Parameter: nameof(client.Id)),
+                (Rule: IsInvalid(client.Firstname), Parameter: nameof(client.Firstname)),
+                (Rule: IsInvalid(client.Lastname), Parameter: nameof(client.Lastname)),
+                (Rule: IsInvalid(client.Email), Parameter: nameof(client.Email)));
+
             return this.storageBroker.InsertClientAsync(client);
+        }
+
+        private dynamic IsInvalid(Guid id) => new
+        {
+            Condition = id == default,
+            Message = "Id is required"
+        };
+
+        private dynamic IsInvalid(string text) => new
+        {
+            Condition = String.IsNullOrWhiteSpace(text),
+            Message = "Text is required"
+        };
+
+        private void Validate(params (dynamic Rule, string Parameter)[] validations)
+        {
+            var invalidClientException = new InvalidClientException();
+
+            foreach ((dynamic rule, string parameter) in validations)
+            {
+                if (rule.Condition)
+                {
+                    invalidClientException.UpsertDataList(
+                        key: parameter,
+                        value: rule.Message);
+                }
+            }
+
+            invalidClientException.ThrowIfContainsErrors();
         }
     }
 }
