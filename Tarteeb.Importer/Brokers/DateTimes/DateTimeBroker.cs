36a6b2cf29e13756afa38bf791196367a131e253
Zafar <icom@MACKBOOK>
2023-09-25 22:12:59 +0300

    BROKERS: DateTimeBroker Initialize

---
+++ Tarteeb.Importer/Brokers/DateTimes/DateTimeBroker.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Tarteeb.Importer.Brokers.DataTimes
+{
+    internal class DateTimeBroker
+    {
+        protected dynamic IsInvalidBirthData(DateTimeOffset birthDate)
+        {
+            var today = DateTimeOffset.UtcNow;
+            var age = today.Year - birthDate.Year;
+
+            if (birthDate.Month >= today.Month && 
+                birthDate.Day >= today.Day)
+                age--;
+
+            return new
+            {
+                Condition = age < 6,
+                Message = "User must be 6 years old or older"
+            };
+        }
+    }
+}
