c5193602046e9bdd55290da14fea7b254cb1fda3
Zafar <icom@MACKBOOK>
2023-09-25 22:50:16 +0300

    POC: Add Client(WIP)

--- Tarteeb.Importer/Services/Clients/ClientService.cs
+++ Tarteeb.Importer/Services/Clients/ClientService.cs
@@ -1,10 +1,13 @@
-﻿//=================================
+﻿
+//=================================
 // Copyright (c) Tarteeb LLC.
 // Powering True Leadership
 //===============================
 
 using System;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using Tarteeb.Importer.Brokers.DataTimes;
 using Tarteeb.Importer.Brokers.Logging;
 using Tarteeb.Importer.Brokers.Storages;
 using Tarteeb.Importer.Models.Clients;
@@ -12,7 +15,7 @@ using Tarteeb.Importer.Models.Clients.Exceptions;
 
 namespace Tarteeb.Importer.Services.Clients
 {
-    internal class ClientService
+    internal class ClientService : DateTimeBroker
     {
         private readonly StorageBroker storageBroker;
         private readonly LoggingBroker loggingBroker;
@@ -36,6 +39,11 @@ namespace Tarteeb.Importer.Services.Clients
                 (Rule: IsInvalid(client.Lastname), Parameter: nameof(client.Lastname)),
                 (Rule: IsInvalid(client.Email), Parameter: nameof(client.Email)));
 
+            Validate(
+                (Rule: IsInvalidBirthData(client.BirthDate), Parameter: nameof(client.BirthDate)),
+                (Rule: IsInvalidEmail(client.Email), Parameter: nameof(client.Email)),
+                (Rule: IsInvalidPhoneNumber(client.PhoneNumber), Parameter: nameof(client.PhoneNumber)));
+
             return this.storageBroker.InsertClientAsync(client);
         }
 
@@ -49,6 +57,18 @@ namespace Tarteeb.Importer.Services.Clients
         {
             Condition = String.IsNullOrWhiteSpace(text),
             Message = "Text is required"
+        };
+
+        private dynamic IsInvalidEmail(string email) => new
+        {
+            Condition = Regex.IsMatch(email, @"^(.+)@(.+)$"),
+            Message = "Email is invalide"
+        };
+
+        private dynamic IsInvalidPhoneNumber(string phoneNumber) => new
+        {
+            Condition = Regex.IsMatch(phoneNumber, "^\\+?[1-9][0-9]{7,14}$"),
+            Message = "Phone number is invalide"
         };
 
         private void Validate(params (dynamic Rule, string Parameter)[] validations)
