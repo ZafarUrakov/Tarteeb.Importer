4784890f592ed43b4b95c9cf1fd80b9ae3f5233b
Your Name <you@example.com>
2023-09-30 18:12:05 +0500

    EXPOSERS: Exceptions Handle

--- Tarteeb.Importer/Program.cs
+++ Tarteeb.Importer/Program.cs
@@ -4,10 +4,15 @@
 //===============================
 
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Tarteeb.Importer.Brokers.DataTimes;
+using Tarteeb.Importer.Brokers.Loggings;
 using Tarteeb.Importer.Brokers.Storages;
 using Tarteeb.Importer.Models.Clients;
+using Tarteeb.Importer.Models.Clients.Exceptions;
 using Tarteeb.Importer.Services.Clients;
 
 namespace Tarteeb.Importer
@@ -20,20 +25,65 @@ namespace Tarteeb.Importer
                 storageBroker: new StorageBroker(),
                 dateTimeBroker: new DateTimeBroker());
 
+            var loggingBroker = new LoggingBroker();
+
             var client = new Client
             {
                 Id = Guid.NewGuid(),
                 Firstname = "",
                 Lastname = "",
                 BirthDate = DateTimeOffset.Parse("1/1/2010"),
-                Email = "johndoejcc.com",
                 PhoneNumber = "+1111111111",
                 GroupId = Guid.NewGuid()
             };
 
-            var persistedClient = await clientService.AddClientAsync(client);
+            try
+            {
+                var persistedClient = await clientService.AddClientAsync(client);
 
-            Console.WriteLine(persistedClient.Id);
+                Console.WriteLine(persistedClient.Id);
+
+            }
+            catch (ClientValidationException ClientValidationException)
+            {
+                if (ClientValidationException.InnerException is InvalidClientException invalidClientXeception)
+                {
+                    foreach (DictionaryEntry key in invalidClientXeception.Data)
+                    {
+                        string ErrorSummary = ((List<string>)key.Value)
+                            .Select((string value) => value).
+                            Aggregate((string current, string next) => current + ", " + next);
+
+                        Console.WriteLine(key.Key + " - " + ErrorSummary);
+                    }
+                    Console.WriteLine($"Message: {invalidClientXeception.Message}");
+                }
+                throw;
+            }
+            catch (ClientDependencyValidationException clientDependencyValidationException)
+            {
+                loggingBroker.LoggingError(clientDependencyValidationException);
+
+                throw clientDependencyValidationException;
+            }
+            catch (ClientDependencyException clientDependencyException)
+            {
+                loggingBroker.LoggingError(clientDependencyException);
+
+                throw clientDependencyException;
+            }
+            catch (ClientServiceException clientServiceException)
+            {
+                loggingBroker.LoggingError(clientServiceException);
+
+                throw clientServiceException;
+            }
+            catch (Exception exception)
+            {
+                loggingBroker.LoggingError(exception);
+
+                throw;
+            }
         }
     }
 }
